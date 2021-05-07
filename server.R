f5f4e78dcb5bafa391d71e6966e89d7faaf9aff2
Rafael Vanhoz <36389864+rafaelvanhoz@users.noreply.github.com>
2021-05-06 21:09:38 -0300

    Update server.R

--- server.R
+++ server.R
@@ -1,47 +1,63 @@
-#
-# This is the server logic of a Shiny web application. You can run the 
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-library(ggplot2)
+library(HistData)
 library(dplyr)
-# select columns to be used in the analysis
-diam <- diamonds[,c(1:4,7)]
-# Define server logic required to draw a plot
+library(ggplot2)
+
+#selecting the data
+data(GaltonFamilies)
+
+# 1st step: Pass inches to cm
+gf <- GaltonFamilies
+gf <- gf %>% mutate(father=father*2.54,
+                    mother=mother*2.54,
+                    childHeight=childHeight*2.54)
+
+# linear model
+model1 <- lm(childHeight ~ father + mother + gender, data=gf)
+
 shinyServer(function(input, output) {
-    output$distPlot <- renderPlot({
-        # select diamonds depending of user input
-        diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
-        # build linear regression model
-        fit <- lm( price~carat, diam)
-        # predicts the price 
-        pred <- predict(fit, newdata = data.frame(carat = input$car,
-                                                  cut = input$cut,
-                                                  color = input$col,
-                                                  clarity = input$clar))
-        # Drow the plot using ggplot2
-        plot <- ggplot(data=diam, aes(x=carat, y = price))+
-            geom_point(aes(color = cut), alpha = 0.3)+
-            geom_smooth(method = "lm")+
-            geom_vline(xintercept = input$car, color = "red")+
-            geom_hline(yintercept = pred, color = "green")
-        plot
+    output$pText <- renderText({
+        paste("Father's height is",
+              strong(round(input$father, 1)),
+              "cm, and mother's height is",
+              strong(round(input$father, 1)),
+              "cm, then:")
     })
-    output$result <- renderText({
-        # renders the text for the prediction below the graph
-        diam <- filter(diamonds, grepl(input$cut, cut), grepl(input$col, color), grepl(input$clar, clarity))
-        fit <- lm( price~carat, diam)
-        pred <- predict(fit, newdata = data.frame(carat = input$car,
-                                                  cut = input$cut,
-                                                  color = input$col,
-                                                  clarity = input$clar))
-        res <- paste(round(pred, digits = 2), "$")
-        res
+    output$pred <- renderText({
+        df <- data.frame(father=input$father,
+                         mother=input$mother,
+                         gender=factor(input$gender, levels=levels(gf$gender)))
+        ch <- predict(model1, newdata=df)
+        kid <- ifelse(
+            input$gender=="female",
+            "Daugther",
+            "Son"
+        )
+        paste0(em(strong(kid)),
+               "'s predicted height is going to be around ",
+               em(strong(round(ch))),
+               " cm"
+        )
     })
-    
+    output$Plot <- renderPlot({
+        kid <- ifelse(
+            input$gender=="female",
+            "Daugther",
+            "Son"
+        )
+        df <- data.frame(father=input$father,
+                         mother=input$mother,
+                         gender=factor(input$gender, levels=levels(gf$gender)))
+        ch <- predict(model1, newdata=df)
+        yvals <- c("Father", kid, "Mother")
+        df <- data.frame(
+            x = factor(yvals, levels = yvals, ordered = TRUE),
+            y = c(input$father, ch, input$mother))
+        ggplot(df, aes(x=x, y=y, color=c("Grey", "green", "black"), fill=c("Grey", "green", "black"))) +
+            geom_bar(stat="identity", width=0.5) +
+            xlab("") +
+            ylab("Height (cm)") +
+            theme_minimal() +
+            theme(legend.position="none")
+    })
 })
